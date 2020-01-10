36c784b87202d462e0d650e49c4dc4a74171df05
vinothautomation <vinothparamas@gmail.com>
2020-01-10 18:31:57 +0530

    code added

---
+++ src/org/test/run/DayOne.java
@@ -0,0 +1,13 @@
+package org.test.run;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DayOne {
+public static void main(String[] args) {
+	System.setProperty("webdriver.chrome.driver", "F:\\SOFTWARES\\selenium\\WORKED_FILED\\eclipse-workspace_selenium_projrcts\\GIT\\driver\\chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+	driver.get("https:www.gmail.com/");
+	driver.quit();
+}
+}
